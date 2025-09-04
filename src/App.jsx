edd7c2f305a10756006987db06966eda95642ad2
OBTITAN <emmanuel7ninsin@gmail.com>
2025-09-04 01:08:20 +0000

    initialized React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div class="text-3xl font-bold bg-green-500 ">
+      Hello World
+    </div>
+  )
+}
+
+export default App
