6a38b2381ca766dd01fd2816c8e321a7af4f3e60
OBTITAN <emmanuel7ninsin@gmail.com>
2025-09-09 23:03:55 +0000

    Made improvements to WeatherCard responsive desgn and made progress with API calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import WeatherDetails from './components/WeatherDetails'
 import WeatherCard from './components/WeatherCard';
 
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const BASE_URL= `https://api.weatherstack.com/current`
+
+ const API_DETAILS ={
+    method: 'GET',
+    header: {
+      Authorization: `BEARER${API_KEY}`,
+      accept: 'application/json',
+    }
+  }
+ 
 function App() {
+  const [weatherData, setWeatherData] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const fetchWeatherData = async (query) => {
+  try{
+      setIsLoading(true);
+      const response = await fetch(`${BASE_URL}?access_key=${API_KEY}&query=${query}`);
+
+      if(!response.ok){
+        console.log('Could not fetch weather data');
+      }
+
+      const data =  await response.json;
+
+      console.log(data);
+
+    } catch(error){
+    console.error("Error fetching weather data:", error);
+  } finally {
+    setIsLoading(false);
+  }
+}
+
+useEffect(() =>{
+  fetchWeatherData('New York');
+},[])
+
   let currentDate = new Date().toLocaleDateString();
   const weatherState = 'Cloudy';
 
@@ -14,9 +54,10 @@ function App() {
           </div>
           <div class='bg-[#D3D3D3] mb-5 p-10 rounded-4xl h-50 flex justify-center items-center space-x-3'>
               <div class='text-white text-left ml-1 mr-auto'>
+                <p class='font-extrabold text-3xl'>Some city</p>
                 <p>{currentDate}</p>
                 <p class='font-bold'>{weatherState}</p>
-                <h1> Exact Temp would be here</h1>
+                <h1>API TEMP DATA</h1>
               </div>
 
               <div class=',mr-3 ml-auto'>
@@ -39,13 +80,13 @@ function App() {
                  <WeatherDetails weatherMeasure = 'Rain' imagePath = 'precepitation.svg' altImageTxt = 'Precipitation Image'/>
             </div>
 
-             <div class='flex justify-items-start mb-5 ml-12 space-x-20 text-left text-lg font-extrabold'>
+             <div class='flex justify-items-start flex-wrap mb-5 ml-12 space-x-20 text-left text-lg font-extrabold'>
                <p>Today</p>
                <p>Tomorrow</p>
                <p>Next 3 Days</p>
             </div>
 
-            <div class='min-sm:columns-1 min-sm:mx-auto md:flex md:justify-around '>
+            <div class='flex justify-around flex-wrap'>
               <WeatherCard weatherIcon='sun-cloud-icon.svg' iconAltText='sun and cloud icon' />
               <WeatherCard weatherIcon='sun-cloud-fast-wind-icon.svg' iconAltText='sun and cloud with wind icon' />
               <WeatherCard weatherIcon='cloud-angled-rain-zap-icon.svg' iconAltText='thumdercloud with rain icon' />
--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -4,7 +4,7 @@ const WeatherDetails = ({weatherMeasure, imagePath, altImageTxt}) => {
   return (
     <div class= 'flex flex-col justify-center items-center'>
         <img src= {imagePath} alt={altImageTxt} class='h-[70px] w-[70px] mb-3'/>
-        <p>API data here</p>
+        <p>18</p>
         <p class='text-white font-extrabold text-2xl'>{weatherMeasure}</p>
     </div>
   )
