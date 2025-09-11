07c524d6f34211e3501cd38ac06133568512f1ea
OBTITAN <emmanuel7ninsin@gmail.com>
2025-09-11 01:58:21 +0000

    Applied weather API and changed class to className for proper jsx manipulation

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,36 @@ import WeatherCard from './components/WeatherCard';
 
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-const BASE_URL= `https://api.weatherstack.com/current`
+const BASE_URL= `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`
 
- const API_DETAILS ={
-    method: 'GET',
-    header: {
-      Authorization: `BEARER${API_KEY}`,
-      accept: 'application/json',
-    }
-  }
  
 function App() {
-  const [weatherData, setWeatherData] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const fetchWeatherData = async (query) => {
-  try{
-      setIsLoading(true);
-      const response = await fetch(`${BASE_URL}?access_key=${API_KEY}&query=${query}`);
-
-      if(!response.ok){
-        console.log('Could not fetch weather data');
+  const fetchWeatherData = async (locationQuery) => {
+      try {
+          setIsLoading(true);
+          const response = await fetch(`${BASE_URL}${locationQuery}?unitGroup=us&key=${API_KEY}&contentType=json`);
+ 
+      if (!response.ok) {
+        setError('Could not fetch weather data');
+        console.log(response.status, response.statusText);
+      } else {
+        const data = await response.json();
+        console.log(data);
       }
-
-      const data =  await response.json;
-
-      console.log(data);
-
-    } catch(error){
-    console.error("Error fetching weather data:", error);
-  } finally {
-    setIsLoading(false);
-  }
+    } catch (error) {
+      setError('An error occurred while fetching weather data');
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
 }
 
 useEffect(() =>{
-  fetchWeatherData('New York');
+  fetchWeatherData('London');
 },[])
 
   let currentDate = new Date().toLocaleDateString();
@@ -50,29 +43,29 @@ useEffect(() =>{
   return(
     <div>
           <div>
-            <p class='font-extrabold text-left text-4xl mb-10'>Welcome</p>
+            <p className='font-extrabold text-left text-4xl mb-10'>Welcome</p>
           </div>
-          <div class='bg-[#D3D3D3] mb-5 p-10 rounded-4xl h-50 flex justify-center items-center space-x-3'>
-              <div class='text-white text-left ml-1 mr-auto'>
-                <p class='font-extrabold text-3xl'>Some city</p>
+          <div className='bg-[#D3D3D3] mb-5 p-10 rounded-4xl h-50 flex justify-center items-center space-x-3'>
+              <div className='text-white text-left ml-1 mr-auto'>
+                <p className='font-extrabold text-3xl'>Some city</p>
                 <p>{currentDate}</p>
-                <p class='font-bold'>{weatherState}</p>
+                <p className='font-bold'>{weatherState}</p>
                 <h1>API TEMP DATA</h1>
               </div>
 
-              <div class=',mr-3 ml-auto'>
+              <div className=',mr-3 ml-auto'>
                 {weatherState == 'Cloudy'
-                ?(<img src='Cloudy.svg' alt='cloudy' class='h-[100px] w-[100px]'/>)
+                ?(<img src='Cloudy.svg' alt='cloudy' className='h-[100px] w-[100px]'/>)
                 :weatherState == 'Sunny'
-                ?(<img src='Sunny.svg' alt='sunny' class='h-[100px] w-[100px]'/>)
+                ?(<img src='Sunny.svg' alt='sunny' className='h-[100px] w-[100px]'/>)
                 :weatherState == 'Rainy'
-                ?(<img src='Rainy.svg' alt='rainy' class='h-[100px] w-[100px]'/>)
-                :(<img src='Cloudy.svg' alt='cloudy' class='h-[100px] w-[100px]'/>)
+                ?(<img src='Rainy.svg' alt='rainy' className='h-[100px] w-[100px]'/>)
+                :(<img src='Cloudy.svg' alt='cloudy' className='h-[100px] w-[100px]'/>)
                 }
               </div>
             </div>
 
-            <div class='bg-[#D3D3D3] p-20 mb-7 rounded-4xl h-70 flex justify-around items-center space-x-3'>
+            <div className='bg-[#D3D3D3] p-20 mb-7 rounded-4xl h-70 flex justify-around items-center space-x-3'>
                  <WeatherDetails weatherMeasure = 'Wind' imagePath = 'fast-wind.svg' altImageTxt = 'Wind Image'/>
 
                  <WeatherDetails weatherMeasure = 'Humidity' imagePath = 'humidity.svg' altImageTxt = 'Humidity Image'/>
@@ -80,13 +73,13 @@ useEffect(() =>{
                  <WeatherDetails weatherMeasure = 'Rain' imagePath = 'precepitation.svg' altImageTxt = 'Precipitation Image'/>
             </div>
 
-             <div class='flex justify-items-start flex-wrap mb-5 ml-12 space-x-20 text-left text-lg font-extrabold'>
+             <div className='flex justify-items-start flex-wrap mb-5 ml-12 space-x-20 text-left text-lg font-extrabold'>
                <p>Today</p>
                <p>Tomorrow</p>
                <p>Next 3 Days</p>
             </div>
 
-            <div class='flex justify-around flex-wrap'>
+            <div className='flex justify-around flex-wrap'>
               <WeatherCard weatherIcon='sun-cloud-icon.svg' iconAltText='sun and cloud icon' />
               <WeatherCard weatherIcon='sun-cloud-fast-wind-icon.svg' iconAltText='sun and cloud with wind icon' />
               <WeatherCard weatherIcon='cloud-angled-rain-zap-icon.svg' iconAltText='thumdercloud with rain icon' />
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 //Each weather card gives the general warning for the day
 const WeatherCard = ({weatherIcon, iconAltText}) => {
   return (
-    <div class='bg-[#D3D3D3] h-60 w-50 rounded-4xl flex flex-col justify-center items-center m-5 p-5 space-y-3'>
+    <div className='bg-[#D3D3D3] h-60 w-50 rounded-4xl flex flex-col justify-center items-center m-5 p-5 space-y-3'>
       <p>Stuff</p>
-      <img src={weatherIcon} alt={iconAltText} class='h-[70px] w-[70px]'/>
+      <img src={weatherIcon} alt={iconAltText} className='h-[70px] w-[70px]'/>
       <p>Some temperature in degrees celcuis</p>
     </div>
   )
